03eb4b702bd4901644b9943f59aebeafd57d16a8
divyendra <dnaidu@salesforce.com>
2020-12-20 02:14:15 -0600

    Coding challenge 2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,57 @@
 'use strict';
-function logger(name) {
+/* function logger(name) {
     console.log(`My name is ${name}`);
 }
+logger('Divyendra');
 
-logger();
+//function declaration - hoisted- can be called before it is declared
+function calcAge1(birthYear) {
+    return 2021 - birthYear;
+}
+const age1 = calcAge1(1983);
+
+//function expression
+const calcAge2 = function(birthYear) {
+    return 2021 - birthYear;
+}
+const age2 = calcAge2(1989);
+
+console.log(age1, age2);
+
+//arrow function
+
+const calcAge3 = birthYear => 2021 - birthYear;
+const age3 = calcAge3(1992);
+console.log(age3);
+
+const yearsUntilResurement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years`;
+}
+console.log(yearsUntilResurement(1991, 'Divyendra')); */
+
+//Coding challenge 2
+const calcAvg = scores => {
+    return (scores[0] + scores[1] + scores[2])/3;
+}
+function Team(name, scores) {
+    this.name = name;
+    this.scores = scores;
+    this.average = calcAvg(scores);
+}
+const dolphins = new Team('Dolphins', [44,23,71]);
+const koalas = new Team('Koalas', [1,2,3]);
+
+const checkWinner = function(avgDolphins, avgKoalas) {
+    let winnerLog;
+    if(avgDolphins !== avgKoalas) {
+        if(avgDolphins >= avgKoalas * 2) {
+            winnerLog = `${dolphins.name} win (${avgDolphins} v ${avgKoalas})`;
+        } else if(avgKoalas >= avgDolphins * 2) {
+            winnerLog = `${koalas.name} win (${avgKoalas} v ${avgDolphins})`;
+        }
+    } return winnerLog;
+}
+const winnerLog = checkWinner(dolphins.average, koalas.average);
+console.log(winnerLog);
