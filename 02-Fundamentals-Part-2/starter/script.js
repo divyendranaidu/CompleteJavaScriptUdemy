fb981415ce5202f03479140d94476fd23cff9c7d
divyendra <dnaidu@salesforce.com>
2020-12-23 00:34:51 -0600

    completed fundamentals

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -2,7 +2,7 @@
 /* function logger(name) {
     console.log(`My name is ${name}`);
 }
-logger('Divyendra');
+logger('test');
 
 //function declaration - hoisted- can be called before it is declared
 function calcAge1(birthYear) {
@@ -55,7 +55,7 @@ const checkWinner = function(avgDolphins, avgKoalas) {
 }
 const winnerLog = checkWinner(dolphins.average, koalas.average);
 console.log(winnerLog); */
-
+/* 
 const calcTip = function(billAmount) {
     const tipPercent = (billAmount >= 50 && billAmount <= 300)? 0.15 : 0.20;
     return billAmount * tipPercent;
@@ -72,3 +72,95 @@ bills.forEach(bill => {
 console.log(`Bills = ${bills};
 Tips = ${tips};
 Total = ${total}`);
+ */
+//arry.push(a) - add element a at the end of the array
+//array.unshift(a) - add element at the beginning of the array
+//array.pop() - removes the last element - returns the popped element
+//array.shift() - remove the first element - returned the removed element
+//array.indexOf(a) - returns an int value with index number of element a in the array
+//array.includes(a) - returns true if a is in the array else returns false. Uses strict equality
+
+/* 
+const divyendra = {
+    firstName: 'Divyendra',
+    lastName: 'Naidu',
+    birthYear: 1983,
+    job: 'Architect',
+    friends: ['Hemant', 'Juhi', 'Chitra'],
+    hasDriversLicense : false,
+    /* calcAge : function(birthYear) {
+        return 2020 - birthYear;
+    } 
+    calcAge: function () {
+        this.age = 2020 - this.birthYear;
+        return this.age;
+    },
+
+    getSummary : function() {
+        return `${this.firstName} is a ${this.calcAge()} year old ${divyendra.job} 
+        and he has ${this.hasDriversLicense ? 'a' : 'no'} drivers license`;
+
+    }
+}
+//divyendra.calcAge();
+
+//console.log(divyendra.calcAge(1983));
+//console.log(divyendra['calcAge'](1983));
+//const exp = (divyendra.hasDriversLicense)? 'a' : 'no';
+//const output = `${divyendra.firstName} is a ${divyendra.age} year old ${divyendra.job}, and he has ${exp} drivers license`;
+//console.log(output);
+
+console.log(divyendra.getSummary()); */
+
+/* const person = {
+    name : this.name,
+    mass : this.mass,
+    height : this.height,
+    calcBMI : function() {
+        this.bmi = this.mass/(this.height ** 2);
+        return this.bmi;
+    }
+}
+const john = new person(name = 'John', mass =  92, height = 1.95);
+const mark = new person(name = 'Mark', mass =  78, height = 1.69);
+
+console.log(john.bmi + '' + mark.bmi); */
+
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const total = [];
+
+// const calcTip = function(billAmount) {
+//     const tipPercent = (billAmount >= 50 && billAmount <= 300)? 0.15 : 0.20;
+//     return billAmount * tipPercent;
+// }
+
+// for(let i = 0; i < bills.length; i++) {
+//     let tip = calcTip(bills[i]);
+//     tips.push(tip);
+//     total.push(tip + bills[i]);
+// }
+// const calcAverage = function(numArray) {
+//     let total = 0;
+//     for(let element of numArray) {
+//         total += element;
+//     }
+//     return total/numArray.length;
+// }
+// //
+// console.log(tips);
+// console.log(total);
+// console.log(total);
+
+const printForecast = function(forecastedMaxTempArray) {
+    let output = '';
+    for(let temp of forecastedMaxTempArray) {
+        output += `${temp}°C in ${forecastedMaxTempArray.indexOf(temp) + 1} days${forecastedMaxTempArray.indexOf(temp) + 1 === forecastedMaxTempArray.length ? '': ', '}`
+    }
+    return output;
+}
+const printThis1 = printForecast([17, 21,23]);
+const printThis2 = printForecast([12, 5, -5, 0, 4]);
+
+console.log(printThis1);
+console.log(printThis2);
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,56 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '👑 Correct Number';
+document.querySelector('.number').textContent = 10;
+document.querySelector('.score').textContent = 13;
+document.querySelector('.guess').value = 23;
+*/
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function() {
+    //console.log(document.querySelector('.guess').value);
+    const guess = Number(document.querySelector('.guess').value);
+    if(!guess) {
+        document.querySelector('.message').textContent = 'No number';
+    } else if(guess === secretNumber) {
+        document.querySelector('.message').textContent = '👑 Correct Number';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.check').disabled = true;
+        if(score > Number(document.querySelector('.highscore').textContent)) {
+            document.querySelector('.highscore').textContent = score;
+        }        
+
+    } else if(guess > secretNumber) {
+        
+            document.querySelector('.message').textContent = '👆 Too High';
+            score--;
+            document.querySelector('.score').textContent = score;        
+
+    } else if(guess < secretNumber) {
+        document.querySelector('.message').textContent = '👇 Too Low';
+        score--;
+        document.querySelector('.score').textContent = score;
+    }
+    if(score === 0) {
+        document.querySelector('.check').disabled = true;
+        document.querySelector('.message').textContent = '🤯 You lost!'
+    } 
+
+
+});
+
+document.querySelector('.again').addEventListener('click', function(){
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.number').textContent = secretNumber;
+    score = 20;
+    document.querySelector('.score').textContent = score;   
+    document.querySelector('.check').disabled = false;
+    document.querySelector('.message').textContent = 'Start Guessing...';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.guess').value = '';
+
+});
